70b5bc423383d44b71fe34f9092a8c86b97b8f5b
Alexander <alexander@kuvaev.me>
2016-03-06 12:26:45 +0300

    Added dungeon generation

--- src/game.rs
+++ src/game.rs
@@ -4,6 +4,7 @@ use dungeon_generator::{BSPGenerator, Room, DungeonGenerator, RoomType};
 
 const TEMP_WIDTH: u32 = 800;
 const TEMP_HEIGHT: u32 = 600;
+const TILE_SIZE: u32 = 10;
 
 pub enum TileType {
     None,
@@ -38,12 +39,21 @@ impl Game {
     }
 
     pub fn generate_level(&mut self, seed: &[usize]) {
-        self.rooms = BSPGenerator::default().generate(seed, TEMP_WIDTH, TEMP_HEIGHT);
+        self.rooms = BSPGenerator::default().min_room_splittable_size(100).coridor_width(15).generate(seed, TEMP_WIDTH, TEMP_HEIGHT);
         for room in self.rooms.iter() {
-            match room.room_type {
-                RoomType::BasicRoom => self.tiles.add_tile(room.x as i32, room.y as i32, room.width, room.height, 2, TileType::Floor(1)),
-                RoomType::Coridor => self.tiles.add_tile(room.x as i32, room.y as i32, room.width, room.height, 3, TileType::Floor(1)),
+            let layer_ind = match room.room_type {
+                RoomType::BasicRoom => 2,
+                RoomType::Coridor => 3,
                 _ => unreachable!(),
+            };
+            let mut x = (room.x/TILE_SIZE)*TILE_SIZE;
+            while x < room.x + room.width {
+                let mut y = (room.y/TILE_SIZE)*TILE_SIZE;
+                while y < room.y + room.height {
+                    self.tiles.add_tile(x as i32, y as i32, TILE_SIZE, TILE_SIZE, layer_ind, TileType::Floor(1));
+                    y += TILE_SIZE;
+                }
+                x += TILE_SIZE;
             }
         }
     }
